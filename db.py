5ce4f8d026b403baadf90b646f3a93dc1b847c40
Martin Noya <martinnoya@gmail.com>
2019-03-12 09:30:22 -0300

    Update save messages feature

--- db.py
+++ db.py
@@ -20,18 +20,18 @@ def setup_database():
     conn.close()
 
 
-def store_message(message, username):
+def save_message(message, username):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     date_now = datetime.now().strftime("%Y-%m-%d")
     print("Saving message for date {}, submitted by {}".format(date_now, username))
     try:
-        creation_sql = "INSERT INTO messages('', message, user)".format(date_now)
+        creation_sql = "INSERT INTO messages('{}', '{}', '{}')".format(date_now, message, username)
         print(creation_sql)
         c.execute(creation_sql)
         conn.commit()
         print("Message saved: {}".format(message))
-    except:
-        pass
+    except Exception as e:
+        print("Problem saving message: {}".format(e))
 
     conn.close()
